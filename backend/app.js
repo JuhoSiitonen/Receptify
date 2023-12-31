750be4f5ba87cae008a1107b65633580ddc76763
Juho Siitonen <juho_tuomas@msn.com>
2023-12-31 12:43:11 +0200

    first test of recipy controller

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,5 @@
 const config = require('./utils/config')
+const express = require('express')
 const app = express()
 const cors = require('cors')
 const recipyRouter = require('./controllers/recipies')
--- backend/controllers/recipies.js
+++ backend/controllers/recipies.js
@@ -1,7 +1,17 @@
 const recipyRouter = require("express").Router();
 
+const recipies = [
+  {
+      id: 1,
+      title: 'Test recipe',
+      description: 'Test description',
+      ingredients: 'Test ingredients',
+      instructions: 'Test instructions'
+  }
+]
+
 recipyRouter.get("/", (request, response) => {
-  response.send("<h1>Hello World!</h1>");
+  return response.json(recipies);
 })
 
 module.exports = recipyRouter;
