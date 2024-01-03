ad2a6a88dbf03c1da05765456185eefec0f16035
Juho Siitonen <juho_tuomas@msn.com>
2024-01-03 22:30:01 +0200

    changed mock data format

--- backend/controllers/recipies.js
+++ backend/controllers/recipies.js
@@ -5,9 +5,9 @@ const recipies = [
       id: 1,
       title: 'Test recipe',
       description: 'Test description',
-      ingredients: 'Test ingredients',
+      ingredients: ['Test ingredients', 'Test amount'],
       instructions: 'Test instructions',
-      category: 'Test category',
+      category: ['Test category', 'Test subcategory'],
       date: '2021-01-01T00:00:00.000Z',
       user: 'Test user'
   },
@@ -15,9 +15,9 @@ const recipies = [
       id: 2,
       title: 'Test recipe 2',
       description: 'Test description 2',
-      ingredients: 'Test ingredients 2',
+      ingredients: ['Test ingredients 2', 'Test amount 2'],
       instructions: 'Test instructions 2',
-      category: 'Test category 2',
+      category: ['Test category 2', 'Test subcategory 2'],
       date: '2021-01-02T00:00:00.000Z',
       user: 'Test user 2'
   }
