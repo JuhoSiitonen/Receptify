b6e569ec76ccac666e30433209774b2f3ccbdd25
Juho Siitonen <juho_tuomas@msn.com>
2024-01-05 17:54:08 +0200

    added nodemon to backend dev dependencies, also added sequelize connection to backend start

--- backend/app.js
+++ backend/app.js
@@ -1,3 +1,4 @@
+require('express-async-errors')
 const config = require('./utils/config')
 const express = require('express')
 const app = express()
@@ -5,12 +6,27 @@ const cors = require('cors')
 const recipyRouter = require('./controllers/recipies')
 const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
+const { Sequelize } = require('sequelize')
+require('dotenv').config()
 
 app.use(cors())
 app.use(express.static('build'))
 app.use(express.json())
 app.use(middleware.requestLogger)
 
+const sequelize = new Sequelize(process.env.DATABASE_URL)
+
+const testConnection = async () => {
+  try {
+    await sequelize.authenticate()
+    console.log('Connection has been established successfully.')
+    sequelize.close()
+  } catch (error) {
+    console.error('Unable to connect to the database:', error)
+  }
+}
+
+testConnection()
 
 app.use('/api/recipies', recipyRouter)
 
