f7a88c164e39cd3615a9b9825cea7ce3ed7ff902
Juho Siitonen <juho_tuomas@msn.com>
2024-01-06 22:11:30 +0200

    createRecipy in frontend changed to have same parameters as in backend

--- frontend/src/components/RecipiesView/AddRecipe.jsx
+++ frontend/src/components/RecipiesView/AddRecipe.jsx
@@ -1,14 +1,16 @@
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { useState } from 'react'
 import { createRecipy } from '../../reducers/recipyReducer'
 
 const AddRecipe = () => {
-    const [name, setName] = useState('')
+    const [title, setTitle] = useState('')
+    const [description, setDescription] = useState('')
     const [ingredients, setIngredients] = useState([])
     const [ingredient, setIngredient] = useState('')
     const [instructions, setInstructions] = useState('')
     const [category, setCategory] = useState('')
     const [categories, setCategories] = useState([])
+    const user = useSelector(state => state.user)
 
     const dispatch = useDispatch()
 
@@ -24,9 +26,18 @@ const AddRecipe = () => {
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        console.log(name, ingredients, instructions, categories);
-        dispatch(createRecipy(name, ingredients, instructions, categories))
-        setName('')
+        console.log(title, ingredients, instructions, categories);
+        dispatch(createRecipy({
+            title, 
+            description, 
+            instructions,
+            visible: true,
+            userId: user.id,
+            ingredients, 
+            categories
+        }))
+        setTitle('')
+        setDescription('')
         setIngredients([])
         setInstructions('')
         setCategories('')
@@ -38,7 +49,11 @@ const AddRecipe = () => {
             <form onSubmit={handleSubmit}>
                 <div>
                     Recipy name:
-                    <input value={name} onChange={({ target }) => setName(target.value)} />
+                    <input value={title} onChange={({ target }) => setTitle(target.value)} />
+                </div>
+                <div>
+                    Description:
+                    <input value={description} onChange={({ target }) => setDescription(target.value)} />
                 </div>
                 <div>
                     Ingredients:
