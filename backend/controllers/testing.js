4818982a4c6879939d418525a04613b541d79293
Juho Siitonen <juho_tuomas@msn.com>
2024-01-09 00:24:26 +0200

    updated testing endpoint to empty all database tables

--- backend/controllers/testing.js
+++ backend/controllers/testing.js
@@ -1,9 +1,16 @@
 const testingRouter = require('express').Router()
-const { User, Recipy } = require('../models')
+const { User, Recipy, Rating, Ingredient, Category, RecipyCategory, RecipyIngredient } = require('../models')
 
 testingRouter.post('/reset', async (request, response) => {
     await User.destroy({ where: {} })
     await Recipy.destroy({ where: {} })
+    await Rating.destroy({ where: {} })
+    await Comment.destroy({ where: {} })
+    await Ingredient.destroy({ where: {} })
+    await Category.destroy({ where: {} })
+    await RecipyIngredient.destroy({ where: {} })
+    await RecipyCategory.destroy({ where: {} })
+
     response.status(204).end()
     }
 )
