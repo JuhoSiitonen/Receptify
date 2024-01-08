adb88b19918952777e5d6c5cc5cee112906aa5bc
Juho Siitonen <juho_tuomas@msn.com>
2024-01-09 00:05:55 +0200

    added reducer for notifications

--- frontend/src/reducers/store.js
+++ frontend/src/reducers/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit'
 import recipyReducer from './recipyReducer'
 import userReducer from './userReducer'
+import notificationReducer from './notificationReducer'
 
 const store = configureStore({
     reducer: {
       recipies: recipyReducer,
-      user: userReducer
+      user: userReducer,
+      notification: notificationReducer
     },
   })
   
