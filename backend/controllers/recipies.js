363796f5b3cdfb528a4c46fcc173e584c372ac2b
Juho Siitonen <juho_tuomas@msn.com>
2024-01-09 01:22:25 +0200

    added post endpoint for comments

--- backend/controllers/recipies.js
+++ backend/controllers/recipies.js
@@ -102,4 +102,36 @@ recipyRouter.get("/:id/comments", async (req, res) => {
   }
 });
 
+recipyRouter.post("/:id/comments", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { userId, content } = req.body;
+
+    const user = await User.findByPk(userId);
+    if (!user) {
+      return res.status(404).json({ error: 'User not found' });
+    }
+
+    const recipe = await Recipy.findByPk(id);
+    if (!recipe) {
+      return res.status(404).json({ error: 'Recipe not found' });
+    }
+
+    const comment = await Comment.create({
+      content,
+      userId: user.id, 
+      recipyId: recipe.id, 
+    });
+
+    const returnComment = await Comment.findByPk(comment.id, {
+      include: [
+        { model: User },],});
+
+    return res.status(201).json(returnComment);
+  } catch (error) {
+    console.error('Error creating comment:', error);
+    return res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
 module.exports = recipyRouter
