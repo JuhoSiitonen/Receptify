36f88d8e27f374409690575217b16b860aa7c8af
Juho Siitonen <juho_tuomas@msn.com>
2024-01-11 20:32:12 +0200

    first version of filter component with actual functionality

--- frontend/src/components/Filter/index.jsx
+++ frontend/src/components/Filter/index.jsx
@@ -1,14 +1,34 @@
+import React, { useState } from 'react';
 
-const Filter = ({ filter, setFilter }) => {
-    const handleChange = (event) => {
-        setFilter(event.target.value)
-    }
+const Filter = ({ onFilter }) => {
+  const [filterOption, setFilterOption] = useState('title');
+  const [filterValue, setFilterValue] = useState('');
 
-    return (
-        <div>
-            <input value={filter} onChange={handleChange} />
-        </div>
-    )
-}
+  const handleFilter = () => {
+    onFilter({ option: filterOption, value: filterValue });
+  };
+
+  return (
+    <div>
+      <select
+        value={filterOption}
+        onChange={(e) => setFilterOption(e.target.value)}
+      >
+        <option value="title">Title</option>
+        <option value="username">Username</option>
+        <option value="ingredients">Ingredients</option>
+        <option value="categories">Categories</option>
+      </select>
+      <input
+        type="text"
+        placeholder={`Filter by ${filterOption}`}
+        value={filterValue}
+        onChange={(e) => setFilterValue(e.target.value)}
+      />
+      <button onClick={handleFilter}>Filter</button>
+    </div>
+  );
+};
+
 
 export default Filter
