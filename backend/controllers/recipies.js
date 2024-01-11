7772957393c90e98b1092689fca2a8b2c1dcd93b
Juho Siitonen <juho_tuomas@msn.com>
2024-01-12 01:39:59 +0200

    added preliminary delete and put endpoints for recipies.

--- backend/controllers/recipies.js
+++ backend/controllers/recipies.js
@@ -188,4 +188,112 @@ recipyRouter.post("/:id/rating", async (req, res) => {
   }
 });
 
+recipyRouter.delete("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const recipe = await Recipy.findByPk(id);
+    if (!recipe) {
+      return res.status(404).json({ error: 'Recipe not found' });
+    }
+
+    await recipe.destroy();
+
+    return res.status(204).end();
+  } catch (error) {
+    console.error('Error deleting recipe:', error);
+    return res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
+recipyRouter.put("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { title, description, instructions, date, visible, userId, ingredients, categories } = req.body;
+
+    const user = await User.findByPk(userId);
+    if (!user) {
+      return res.status(404).json({ error: 'User not found' });
+    }
+
+    const recipe = await Recipy.findByPk(id);
+    if (!recipe) {
+      return res.status(404).json({ error: 'Recipe not found' });
+    }
+
+    await recipe.update({
+      title,
+      description,
+      instructions,
+      date,
+      visible,
+      userId: user.id, 
+    });
+
+    for (const ingredientData of ingredients) {
+      const { name, amount } = ingredientData;
+
+      let ingredient = await Ingredient.findOne({ where: { name } });
+      if (!ingredient) {
+        ingredient = await Ingredient.create({ name });
+      }
+
+      let recipeIngredient = await RecipyIngredient.findOne({ where: { recipyId: recipe.id, ingredientId: ingredient.id } });
+      if (!recipeIngredient) {
+        recipeIngredient = await RecipyIngredient.create({
+          amount,
+          visible: true,
+          recipyId: recipe.id, 
+          ingredientId: ingredient.id, 
+        });
+      } else {
+        await recipeIngredient.update({
+          amount,
+          visible: true,
+          recipyId: recipe.id, 
+          ingredientId: ingredient.id, 
+        });
+      }
+    }
+
+    for (const categoryData of categories) {
+      const { name } = categoryData;
+
+      let category = await Category.findOne({ where: { name } });
+      if (!category) {
+        category = await Category.create({ name });
+      }
+
+      let recipeCategory = await RecipyCategory.findOne({ where: { recipyId: recipe.id, categoryId: category.id } });
+      if (!recipeCategory) {
+        recipeCategory = await RecipyCategory.create({
+          visible: true,
+          recipyId: recipe.id, 
+          categoryId: category.id, 
+        });
+      } else {
+        await recipeCategory.update({
+          visible: true,
+          recipyId: recipe.id, 
+          categoryId: category.id, 
+        });
+      }
+    }
+
+    const returnRecipy = await Recipy.findByPk(recipe.id,{
+      include: [
+        { model: User },
+        { model: RecipyIngredient, include: [Ingredient] },
+        { model: RecipyCategory },
+      ],
+    });
+    return res.status(200).json(returnRecipy);
+    
+  } catch (error) {
+    console.error('Error updating recipe:', error);
+    return res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
+
 module.exports = recipyRouter
