d78fcb6ab691d5ca34875d42ebb9d0bbdaf0d8ed
Juho Siitonen <juho_tuomas@msn.com>
2024-01-11 19:54:34 +0200

    updated navbar to show different links whether user is logged in or not.

--- frontend/src/components/NavigationBar/index.jsx
+++ frontend/src/components/NavigationBar/index.jsx
@@ -1,12 +1,24 @@
 import { Link } from 'react-router-dom'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { logout } from '../../reducers/userReducer'
 
 const NavigationBar = () => {
     const dispatch = useDispatch()
+    const user = useSelector(state => state.user)
 
     const handleLogout = () => {    
         dispatch(logout())
+    }
+
+    if (user === null) {
+        return (
+            <div>
+                <Link to="/">Mainpage  </Link>
+                <Link to="/recipes">Recipes  </Link>
+                <Link to="/login">Login  </Link>
+                <Link to="/signup">Sign up  </Link>
+            </div>
+        )
     }
 
     return (
@@ -14,7 +26,6 @@ const NavigationBar = () => {
             <Link to="/">Mainpage  </Link>
             <Link to="/recipes">Recipes  </Link>
             <Link to="/recipes/new">Add recipe  </Link>
-            <Link to="/login">Login  </Link>
             <Link to="/mypage">My page  </Link>
             <Link to="/logout" onClick={handleLogout}>Logout </Link>
         </div>
