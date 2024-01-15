c4a9f8aac41c9e77d684b41104b09ad93f291357
Juho Siitonen <juho_tuomas@msn.com>
2024-01-15 19:23:39 +0200

    updated services in frontend to connect to new comment and rating endpoints

--- frontend/src/services/comments.js
+++ frontend/src/services/comments.js
@@ -1,23 +1,23 @@
 import axios from '../util/apiClient'
-const baseUrl = '/api/recipies'
+const baseUrl = '/api/comments'
 
 const getAllComments = async (id) => {
-  const response = await axios.get(`${baseUrl}/${id}/comments`)
+  const response = await axios.get(`${baseUrl}/${id}`)
   return response.data
 }
 
 const create = async (id, newObject) => {
-  const response = await axios.post(`${baseUrl}/${id}/comments`, newObject)
+  const response = await axios.post(`${baseUrl}/${id}`, newObject)
   return response.data
 }
 
 const update = async (id, newObject) => {
-  const request = await axios.put(`${baseUrl}/${id}/comments`, newObject)
+  const request = await axios.put(`${baseUrl}/${id}`, newObject)
   return request.data
 }
 
 const deleteComment = async (id) => {
-  const request = await axios.delete(`${baseUrl}/${id}/comments`)
+  const request = await axios.delete(`${baseUrl}/${id}`)
   return request.data
 }
 
--- frontend/src/services/rating.js
+++ frontend/src/services/rating.js
@@ -1,23 +1,23 @@
 import axios from '../util/apiClient'
-const baseUrl = '/api/recipies'
+const baseUrl = '/api/rating'
 
 const getRatingAverage = async (id) => {
-  const response = await axios.get(`${baseUrl}/${id}/rating`)
+  const response = await axios.get(`${baseUrl}/${id}`)
   return response.data
 }
 
 const create = async (id, newObject) => {
-  const response = await axios.post(`${baseUrl}/${id}/rating`, newObject)
+  const response = await axios.post(`${baseUrl}/${id}`, newObject)
   return response.data
 }
 
 const update = async (id, newObject) => {
-  const request = await axios.put(`${baseUrl}/${id}/rating`, newObject)
+  const request = await axios.put(`${baseUrl}/${id}`, newObject)
   return request.data
 }
 
 const deleteRating = async (id) => {
-  const request = await axios.delete(`${baseUrl}/${id}/rating`)
+  const request = await axios.delete(`${baseUrl}/${id}`)
   return request.data
 }
 
