63ca1818e4fa2a5b29e5787d570570432b7bf49a
Juho Siitonen <juho_tuomas@msn.com>
2024-01-15 19:49:18 +0200

    added updated and delete endpoints for comments

--- backend/controllers/comments.js
+++ backend/controllers/comments.js
@@ -17,7 +17,7 @@ commentRouter.get("/:id", async (req, res) => {
     }
   });
 
-  commentRouter.post("/:id", async (req, res) => {
+commentRouter.post("/:id", async (req, res) => {
     try {
       const { id } = req.params;
       const { userId, content } = req.body;
@@ -47,6 +47,61 @@ commentRouter.get("/:id", async (req, res) => {
       return res.status(201).json(returnComment);
     } catch (error) {
       console.error('Error creating comment:', error);
+      return res.status(500).json({ error: 'Internal server error' });
+    }
+  });
+
+commentRouter.put("/:id", async (req, res) => {
+    try {
+      const { id } = req.params;
+      const { userId, content } = req.body;
+  
+      const user = await User.findByPk(userId);
+      if (!user) {
+        return res.status(404).json({ error: 'User not found' });
+      }
+  
+      const recipe = await Recipy.findByPk(id);
+      if (!recipe) {
+        return res.status(404).json({ error: 'Recipe not found' });
+      }
+  
+      const existingComment = await Comment.findOne({
+        where: { userId: user.id, recipyId: recipe.id },
+      });
+  
+      if (!existingComment) {
+        return res.status(404).json({ error: 'Comment not found' });
+      }
+  
+      existingComment.comment = content;
+      await existingComment.save();
+  
+      const returnComment = await Comment.findByPk(existingComment.id, {
+        include: [
+          { model: User },],});
+  
+      return res.status(200).json(returnComment);
+    } catch (error) {
+      console.error('Error updating comment:', error);
+      return res.status(500).json({ error: 'Internal server error' });
+    }
+  });
+
+commentRouter.delete("/:id", async (req, res) => {
+    try {
+      const { id } = req.params;
+  
+      const comment = await Comment.findByPk(id);
+      if (!comment) {
+        return res.status(404).json({ error: 'Comment not found' });
+      }
+  
+      await comment.destroy();
+  
+      return res.status(204).end();
+    } catch (error) {
+      console.error('Error deleting comment:', error);
       return res.status(500).json({ error: 'Internal server error' });
     }
   });
