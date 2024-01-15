d5397bb05ad5b01038eb220cdd9cf0a3c44c20d1
Juho Siitonen <juho_tuomas@msn.com>
2024-01-15 16:28:13 +0200

    fixed rating component radio buttons

--- frontend/src/components/RecipiesView/Rating/index.jsx
+++ frontend/src/components/RecipiesView/Rating/index.jsx
@@ -20,15 +20,15 @@ const Rating = ({ recipyId }) => {
             Rating
             <form onSubmit={handleSubmit}> 
                 <input type="radio" id="1" name="rating" value="1" />
-                <label for="1">1</label>
+                <label htmlFor="1">1</label>
                 <input type="radio" id="2" name="rating" value="2" />
-                <label for="2">2</label>
+                <label htmlFor="2">2</label>
                 <input type="radio" id="3" name="rating" value="3" />
-                <label for="3">3</label>
+                <label htmlFor="3">3</label>
                 <input type="radio" id="4" name="rating" value="4" />
-                <label for="4">4</label>
+                <label htmlFor="4">4</label>
                 <input type="radio" id="5" name="rating" value="5" />
-                <label for="5">5</label>
+                <label htmlFor="5">5</label>
                 <button type="submit">Submit</button>
             </form>
         </div>
