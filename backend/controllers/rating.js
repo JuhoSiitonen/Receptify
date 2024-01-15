54c8234ffd61051f39f76e0e25ac079116ee7a9c
Juho Siitonen <juho_tuomas@msn.com>
2024-01-15 19:34:11 +0200

    added update and delete endpoints to rating

--- backend/controllers/rating.js
+++ backend/controllers/rating.js
@@ -1,4 +1,5 @@
 const ratingRouter = require("express").Router();
+const { default: rating } = require("../../frontend/src/services/rating");
 const { Rating, User, Recipy } = require("../models");
 const { sequelize } = require('../utils/db');
 
@@ -49,6 +50,59 @@ ratingRouter.get("/:id", async (req, res) => {
       return res.status(201).json(returnRating);
     } catch (error) {
       console.error('Error creating rating:', error);
+      return res.status(500).json({ error: 'Internal server error' });
+    }
+  });
+
+ratingRouter.put("/:id", async (req, res) => {
+    try {
+      const { id } = req.params;
+      const { userId, rating } = req.body;
+  
+      const user = await User.findByPk(userId);
+      if (!user) {
+        return res.status(404).json({ error: 'User not found' });
+      }
+  
+      const recipe = await Recipy.findByPk(id);
+      if (!recipe) {
+        return res.status(404).json({ error: 'Recipe not found' });
+      }
+  
+      const existingRating = await Rating.findOne({
+        where: { userId: user.id, recipyId: recipe.id },
+      });
+  
+      if (!existingRating) {
+        return res.status(404).json({ error: 'Rating not found' });
+      }
+  
+      existingRating.rating = rating;
+      await existingRating.save();
+  
+      const returnRating = await Rating.findByPk(existingRating.id);
+  
+      return res.status(200).json(returnRating);
+    } catch (error) {
+      console.error('Error updating rating:', error);
+      return res.status(500).json({ error: 'Internal server error' });
+    }
+  });
+
+  ratingRouter.delete("/:id", async (req, res) => {
+    try {
+      const { id } = req.params;
+  
+      const rating = await Rating.findByPk(id);
+      if (!rating) {
+        return res.status(404).json({ error: 'Rating not found' });
+      }
+  
+      await rating.destroy();
+  
+      return res.status(204).end();
+    } catch (error) {
+      console.error('Error deleting rating:', error);
       return res.status(500).json({ error: 'Internal server error' });
     }
   });
