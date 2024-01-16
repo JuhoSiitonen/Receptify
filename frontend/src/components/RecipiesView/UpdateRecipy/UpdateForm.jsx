71fe20e23283bfc7f5bdc77a5ac0e8882f62b12f
Juho Siitonen <juho_tuomas@msn.com>
2024-01-16 17:40:15 +0200

    basic update form that shows old ingredients, not yet functional

--- frontend/src/components/RecipiesView/UpdateRecipy/UpdateForm.jsx
+++ frontend/src/components/RecipiesView/UpdateRecipy/UpdateForm.jsx
@@ -1,6 +1,44 @@
-
+import { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { updateRecipy } from '../../../reducers/recipyReducer'
 
 const UpdateForm = ({ recipy }) => {
+    const [title, setTitle] = useState('')
+    const [description, setDescription] = useState('')
+    const [ingredients, setIngredients] = useState([])
+    const [ingredient, setIngredient] = useState({ name: '', amount: '' });
+    const [instructions, setInstructions] = useState('')
+    const [category, setCategory] = useState('')
+    const [categories, setCategories] = useState([])
+
+    const dispatch = useDispatch()
+    const user = useSelector(state => state.user)
+
+    const setupFields = () => {
+        setTitle(recipy.title)
+        setDescription(recipy.description)
+        setIngredients(recipy.recipy_ingredients)
+        setInstructions(recipy.instructions)
+        setCategories(recipy.recipy_categories)
+    }
+
+    const addIncredient = () => {
+        setIngredients([...ingredients, ingredient]);
+        setIngredient({ name: '', amount: '' });
+    };
+
+    const addCategory = () => {
+        setCategories([...categories, category])
+        setCategory('')
+    }
+
+    const deleteIngredient = (ingredient) => {
+        setIngredients(ingredients.filter(i => i.id !== ingredient.id))
+    }
+
+    const deleteCategory = (category) => {
+        setCategories(categories.filter(c => c.id !== category.id))
+    }
 
     const handleSubmit = (event) => {
         event.preventDefault()
@@ -9,28 +47,51 @@ const UpdateForm = ({ recipy }) => {
     }
     return (
         <div>
+            {!title && setupFields()}
+            <h1>Update Recipy</h1>
             <form onSubmit={handleSubmit}>
                 <div>
                     Recipy name:
-                    <input type="text" name="title" defaultValue={recipy.title} />
+                    <input value={title} onChange={({ target }) => setTitle(target.value)} />
                 </div>
                 <div>
                     Description:
-                    <input type="text" name="description" defaultValue={recipy.description} />
+                    <input value={description} onChange={({ target }) => setDescription(target.value)} />
                 </div>
                 <div>
                     Ingredients:
-                    <input type="text" name="ingredients" defaultValue={recipy.recipy_ingredients.map(ingredient => ingredient.name)} />
+                    <input value={ingredient.name} onChange={({ target }) => setIngredient({ ...ingredient, name: target.value })}
+                     placeholder="Ingredient name" />
+                    <input value={ingredient.amount} onChange={({ target }) => setIngredient({ ...ingredient, amount: target.value })}
+                     placeholder="Amount" />
+                    <button onClick={addIncredient} type="button">add</button>
+                </div>
+                <div>
+                {ingredients.map(ingredient => (
+                    <li key={`${ingredient.id}-${ingredient.ingredient.id}`}>
+                        {ingredient.amount} of {ingredient.ingredient.name}
+                        <button onClick={() => deleteIngredient(ingredient)} type="button">delete</button>
+                    </li>
+                ))}
                 </div>
                 <div>
                     Instructions:
-                    <input type="text" name="instructions" defaultValue={recipy.instructions} />
+                    <input value={instructions} onChange={({ target }) => setInstructions(target.value)} />
                 </div>
                 <div>
-                    Categories:
-                    <input type="text" name="categories" defaultValue={recipy.recipy_categories.map(category => category.name)} />
+                    Category:
+                    <input value={category} onChange={({ target }) => setCategory(target.value)} />
+                    <button onClick={addCategory} type="button">add</button>
                 </div>
-                <button type="submit">Update</button>
+                <div>Categories: 
+                    {categories.map(category => {
+                        <li key={category.id}>
+                            {category.category.name} 
+                            <button onClick={() => deleteCategory(category)} type="button">delete</button>
+                        </li>
+                    })}
+                </div>
+                <input type="submit" value="Submit" />
             </form>
         </div>
     )
