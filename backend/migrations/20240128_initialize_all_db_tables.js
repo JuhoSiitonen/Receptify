6df5dc26181388172812f5cf41bba6a4874b8d24
Juho Siitonen <juho_tuomas@msn.com>
2024-01-28 23:35:07 +0200

    started with db migrations

---
+++ backend/migrations/20240128_initialize_all_db_tables.js
@@ -0,0 +1,13 @@
+const { DataTypes } = require('sequelize')
+
+
+module.exports = { 
+    up: async ({ context: queryInterface }) => {
+        await queryInterface.createTable('users', {
+
+        })
+    },
+    down: async ({ context: queryInterface }) => {
+        await queryInterface.dropTable('users')
+    }
+}
