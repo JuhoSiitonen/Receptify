877e50ebc2f7be9d5b8dbee28e5f559f96d60c80
Juho Siitonen <juho_tuomas@msn.com>
2024-02-01 23:55:09 +0200

    moved MSW server updates between tests to jest.setup

--- frontend/jest.setup.js
+++ frontend/jest.setup.js
@@ -1,1 +1,8 @@
 globalThis.fetch = require('isomorphic-fetch');
+import { server } from './src/tests/testServer.js'
+
+beforeAll(() => server.listen())
+
+afterEach(() => server.resetHandlers())
+
+afterAll(() => server.close())
--- frontend/src/tests/testServer.js
+++ frontend/src/tests/testServer.js
@@ -3,8 +3,3 @@ import { handlers } from './handlers'
 
 export const server = setupServer(...handlers)
 
-beforeAll(() => server.listen())
-
-afterEach(() => server.resetHandlers())
-
-afterAll(() => server.close())
