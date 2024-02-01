dbc90ab1b9c9f77aec0883efa786f3a9a530a01b
Juho Siitonen <juho_tuomas@msn.com>
2024-02-01 18:18:44 +0200

    added test-util to create a custom render function with redux store and router components

---
+++ frontend/src/util/test-utils.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import { render } from '@testing-library/react'
+import { Provider } from 'react-redux'
+import { StaticRouter, BrowserRouter } from 'react-router-dom'
+import reduxStore from '../reducers/store'
+
+export function renderWithProviders(
+    ui,
+  {
+    store = reduxStore,
+    ...renderOptions
+  } = {}
+) {
+  function Wrapper({ children }) {
+    return (
+        <Provider store={store}>
+          <BrowserRouter>{children}</BrowserRouter>
+        </Provider>
+    )
+  }
+  return { store, ...render(ui, { wrapper: Wrapper, ...renderOptions }) }
+}
+
+export const AllTheProviders = ({children}) => {
+    return (
+      <BrowserRouter>
+        <Provider store={reduxStore}>
+          {children}
+        </Provider>
+      </BrowserRouter>
+    ) 
+  }
+
+  export const customRender = (ui, options) =>
+  render(ui, {wrapper: AllTheProviders, ...options})
+   
