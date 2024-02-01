e4c94f87d168e6896e4f391d53a2e52384ed65ff
Juho Siitonen <juho_tuomas@msn.com>
2024-02-01 23:55:42 +0200

    created basis for testing login submit

--- frontend/src/tests/Login.test.jsx
+++ frontend/src/tests/Login.test.jsx
@@ -8,6 +8,10 @@ import { renderWithProviders } from '../util/test-utils'
 import { customRender } from '../util/test-utils'
 import { server } from './testServer'
 
+server.events.on('request:start', ({ request }) => {
+  console.log('MSW intercepted:', request.method, request.url)
+})
+
 
 test('renders content', async () => {
     const mockHandler = jest.fn()
@@ -17,5 +21,30 @@ test('renders content', async () => {
       expect(screen.getByLabelText(/username/i)).toBeVisible();
       expect(screen.getByLabelText(/password/i)).toBeVisible();
   });
+})
+
+test('login form calls onSubmit with right details', async () => {
+    const { container } = renderWithProviders(<Login />)
   
+    await waitFor(() => {
+      expect(screen.getByLabelText(/username/i)).toBeVisible();
+      expect(screen.getByLabelText(/password/i)).toBeVisible();
+    });
+    userEvent.type(screen.getByLabelText(/username/i), 'testuser')
+    userEvent.type(screen.getByLabelText(/password/i), 'testpassword')
+    const loginButton = screen.getByRole('button', { name: /login/i });
+    userEvent.click(loginButton);
+
+    /*
+    expect(await response.json()).toEqual({
+        "id": 1,
+        "username": "John Doe",
+        "password": "password",
+        "admin": false,
+        "visible": true,
+        "createdAt": "2024-02-01T14:12:04.268Z",
+        "updatedAt": "2024-02-01T14:12:04.268Z"
+      }
+    )
+    */
 })
--- frontend/src/tests/handlers.js
+++ frontend/src/tests/handlers.js
@@ -1,8 +1,21 @@
 import { http, HttpResponse, delay } from 'msw'
 
+const baseURL = process.env.REACT_APP_BACKEND_URL
+
 export const handlers = [
-    http.get('/api/login', async () => {
+    http.post(`${baseURL}/api/login`, async ({ request }) => {
       await delay(150)
-      return HttpResponse.json('John Smith')
+      if (request.body.username !== 'john' || request.body.password !== 'password') {
+        return HttpResponse.status(401)
+      }
+      return HttpResponse.json({
+        "id": 1,
+        "username": "John Doe",
+        "password": "password",
+        "admin": false,
+        "visible": true,
+        "createdAt": "2024-02-01T14:12:04.268Z",
+        "updatedAt": "2024-02-01T14:12:04.268Z"
+      })
     })
   ]
