2b894bbf9eb3a7235f34b5bd3e138632bcded080
Juho Siitonen <juho_tuomas@msn.com>
2024-02-13 17:47:50 +0200

    subscribe button is disabled after clicking

--- frontend/src/components/RecipiesView/ViewUser/AddSubscriptionButton.jsx
+++ frontend/src/components/RecipiesView/ViewUser/AddSubscriptionButton.jsx
@@ -4,6 +4,7 @@ import { addSubscription } from '../../../reducers/userReducer'
 
 const AddSubscriptionButton = ({ friendId }) => {
     const [buttonText, setButtonText] = useState('Subscribe')
+    const [isSubscribed, setIsSubscribed] = useState(false)
     const dispatch = useDispatch()
 
 
@@ -11,15 +12,18 @@ const AddSubscriptionButton = ({ friendId }) => {
         e.preventDefault()
         try {
             await dispatch(addSubscription(friendId))
+            setIsSubscribed(true)
             setButtonText('Subscription added')
         }catch (error) {
             console.log(error)
+            setIsSubscribed(true)
         }
     }
 
     return (
         <div>
-            <button onClick={handleAddSubscription}>{buttonText}</button>
+            {!isSubscribed && <button onClick={handleAddSubscription}>{buttonText}</button>}
+            {isSubscribed && <button disabled>{buttonText}</button>}
         </div>
     )
 }
