f02c268ee8f3cfe3ff4b88769a8aa5acca24c363
Juho Siitonen <juho_tuomas@msn.com>
2024-02-25 16:04:21 +0200

    component for sorting recipies

---
+++ frontend/src/components/SortBy/index.jsx
@@ -0,0 +1,34 @@
+import { useState } from 'react';
+
+const SortBy = ({ onSort }) => {
+  const [sortOption, setSortOption] = useState('title');
+  const [sortValue, setSortValue] = useState('ASC');
+
+  const handleSorter = () => {
+    onSort({ option: sortOption, value: sortValue });
+  };
+
+  return (
+    <div>
+      <select
+        value={sortOption}
+        onChange={(e) => setSortOption(e.target.value)}
+      >
+        <option value="cookingTime">Cooking time</option>
+        <option value="averageRating">Rating</option>
+        <option value="createdAt">Created at</option>
+      </select>
+      <select
+        value={sortValue}
+        onChange={(e) => setSortValue(e.target.value)}
+        >
+        <option value="ASC">Ascending</option>
+        <option value="DESC">Descending</option>
+      </select>
+      <button onClick={handleSorter}>Sort</button>
+    </div>
+  );
+};
+
+
+export default SortBy
