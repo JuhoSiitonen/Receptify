eaaf30b89bb1d51b79ae93116431890144968193
Juho Siitonen <juho_tuomas@msn.com>
2024-03-12 21:03:20 +0200

    added css styles to Mainpage

--- frontend/src/components/Mainpage.jsx
+++ frontend/src/components/Mainpage.jsx
@@ -1,13 +1,14 @@
 import { Link } from 'react-router-dom'
+import './Mainpage.css'
 
 const Mainpage = () => {
 
     return (
-        <>
-        <h2>Welcome to Receptify!</h2>
-        <h3>Here you can find recipes and add your own!</h3>
-        <p>Click <Link to="/recipes">here</Link> to see all recipes!</p>
-        </>
+        <div className="mainpage-container">
+          <h2>Welcome to Receptify!</h2>
+          <h3>Here you can find recipes and add your own!</h3>
+          <p>Click <Link to="/recipes">here</Link> to see all recipes!</p>
+        </div>
     )
 }
 
