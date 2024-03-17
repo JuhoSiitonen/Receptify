465e08df40cbdd8ce81bc7693514748d74f6c2b7
Juho Siitonen <juho_tuomas@msn.com>
2024-03-17 19:56:48 +0200

    removed unnecessary code from uploaderwidget

--- frontend/src/components/Uploader/UploaderWidget.jsx
+++ frontend/src/components/Uploader/UploaderWidget.jsx
@@ -1,48 +1,23 @@
-import React from "react";
-import { useState, useCallback } from "react";
+import { useState } from "react";
 import { Widget } from "@uploadcare/react-widget";
 import * as st from './Uploader.scss';
 
 const baseUrl = 'https://ucarecdn.com/'
-
 
 const UploaderWidget = ({ files, onChange}) => {
     const [uploadedFiles, setUploadedFiles] = useState([]);
 
     const handleChange = file => {
         if (!file) {
-          console.log("File removed from widget");
           onChange("")
           setUploadedFiles([])
           return;
         }
         file.done(file => {
-          console.log("File uploaded: ", file.cdnUrl);
           setUploadedFiles([...uploadedFiles, file]);
           onChange(file.uuid);
-          console.log("files state:", files)
-          console.log("the uuid:", file.uuid)
         });
     }
-    
-    /*
-    (file) => {
-        if (file) {
-            console.log(file);
-        }
-        setUploadedFiles([...uploadedFiles, file]);
-        onChange([...files, file]);
-        console.log("uploadedFiles: ", uploadedFiles);
-    }
-    */
-    const handleRemoveClick = useCallback(
-        (uuid) => {
-            onChange(files.filter(f => f.uuid !== uuid))
-            setUploadedFiles(uploadedFiles.filter(f => f.uuid !== uuid))
-        },
-        [files, onChange],
-      );
-
 
     return (
     <div>
@@ -56,9 +31,6 @@ const UploaderWidget = ({ files, onChange}) => {
     <div className={st.previews}>
         {uploadedFiles.map((file) => (
           <div key={file.uuid} className={st.preview} >
-            {console.log(file.uuid)}
-            {console.log(file.cdnUrl)}
-            {console.log("map function running")}
             <img
               className={st.previewImage}
               key={file.uuid}
@@ -67,11 +39,6 @@ const UploaderWidget = ({ files, onChange}) => {
               alt={file.originalFilename || ''}
               title={file.originalFilename || ''}
             />
-            <button
-              className={st.previewRemoveButton}
-              type="button"
-              onClick={() => handleRemoveClick(file.uuid)}
-            >×</button>
           </div>
         ))}
     </div>
