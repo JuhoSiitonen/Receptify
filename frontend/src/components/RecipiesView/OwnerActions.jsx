6c078cbc404814a9c69bd4eff0f1e4adc3d7dbbb
Juho Siitonen <juho_tuomas@msn.com>
2024-03-25 00:17:47 +0200

    new component for owner actions

---
+++ frontend/src/components/RecipiesView/OwnerActions.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { useNavigate } from 'react-router-dom'
+import DeleteButton from './DeleteButton'
+import AllComments from './Comment/AllComments'
+import Togglable from '../Togglable'
+
+const OwnerActions = ({ recipe, user }) => {
+    const navigate = useNavigate()
+
+    return (
+        <div>
+            <button onClick={() => navigate(`/recipes/${recipe.id}`)}>Edit</button>
+            <DeleteButton recipy={recipe} />
+            <Togglable buttonLabel="Show comments" cancelLabel="Hide comments" topCancel={true}>
+                <AllComments user={user} recipy={recipe}/>
+            </Togglable>
+        </div>
+    )
+}
+
+export default OwnerActions
