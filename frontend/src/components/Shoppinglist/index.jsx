987ddb6987d26a8ece297931c3446deed6e59bfa
Juho Siitonen <104775829+JuhoSiitonen@users.noreply.github.com>
2024-03-28 21:56:59 +0000

    shoppinglist component now displays the shoppinglist

--- frontend/src/components/Shoppinglist/index.jsx
+++ frontend/src/components/Shoppinglist/index.jsx
@@ -1,9 +1,31 @@
-
+import { useSelector } from 'react-redux';
 
 const Shoppinglist = () => {
+    const shoppinglist = useSelector(state => state.user.shoppinglist);
+
+    const handleDelete = () => {
+        console.log('delete')
+    }
+
+    if (!shoppinglist) {
+        return <>No items on shoppinglist</>
+    }
+
     return (
         <div>
             <h1>Shoppinglist</h1>
+            <ul>
+                {shoppinglist.map(item => (
+                    <li key={item.recipy}>
+                        {item.map(ingredient => (
+                            <li key={ingredient.ingredient}>
+                                {ingredient.amount} {ingredient.unit} of {ingredient.ingredient}
+                                <button onClick={() => handleDelete()}>Delete</button>
+                            </li>
+                        ))}
+                    </li>
+                ))}
+            </ul>
         </div>
     )
 }
