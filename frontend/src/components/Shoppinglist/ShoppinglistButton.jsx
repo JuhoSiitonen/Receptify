83c2a0e40f1de78467d0472a36324905a07801be
Juho Siitonen <104775829+JuhoSiitonen@users.noreply.github.com>
2024-03-28 21:30:32 +0000

    button for adding to shopping list

---
+++ frontend/src/components/Shoppinglist/ShoppinglistButton.jsx
@@ -0,0 +1,29 @@
+import { useDispatch } from "react-redux";
+
+const ShoppinglistButton = ({ recipy }) => {
+    const dispatch = useDispatch();
+
+    const handleClick = (e) => {
+        e.preventDefault();
+
+        const shoppinglist = {
+            recipy: recipy.id,
+            ingredients: recipy.recipy_ingredients.map(ingredient => {
+                return {
+                    ingredient: ingredient.ingredient.name,
+                    amount: ingredient.amount,
+                    unit: ingredient.unit
+                }
+            })
+        }
+        dispatch(addToShoppinglist(shoppinglist));
+    }
+
+    return (
+        <div>
+            <button onClick={handleClick}>Add to Shoppinglist</button>
+        </div>
+    )
+}
+
+export default ShoppinglistButton;
