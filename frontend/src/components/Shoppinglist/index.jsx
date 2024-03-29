f7875245586cd7446abd7ae3698e783f29a6abee
Juho Siitonen <juho_tuomas@msn.com>
2024-03-29 18:23:33 +0200

    added function to combine same ingredients together

--- frontend/src/components/Shoppinglist/index.jsx
+++ frontend/src/components/Shoppinglist/index.jsx
@@ -10,16 +10,54 @@ const Shoppinglist = () => {
         shoppinglist.filter(item => item.id !== id)
     }
 
+    const combineIngredients = (list) => {
+        const combinedList = [];
+        list.forEach(item => {
+            const existingItemIndex = combinedList.findIndex(elem => elem.ingredient === item.ingredient);
+            if (existingItemIndex !== -1) {
+                combinedList[existingItemIndex].amount = Number(combinedList[existingItemIndex].amount) + Number(convertToBaseUnit(item.amount, item.unit))
+            } else {
+                combinedList.push({ ...item, amount: convertToBaseUnit(item.amount, item.unit) });
+            }
+        });
+        return combinedList;
+    };
+
+    const convertToBaseUnit = (amount, unit) => {
+        switch (unit) {
+            case 'tbsp':
+                return amount * 15; 
+            case 'tsp':
+                return amount * 5; 
+            case 'kg':
+                return amount * 1000; 
+            case 'l':
+                return amount * 1000; 
+            case 'dl':
+                return amount * 100; 
+            case 'cup':
+                return amount * 240;
+            case 'pcs':
+            case '':
+                return amount; 
+            default:
+                return amount; 
+        }
+    };
+
+
     if (!shoppinglist) {
         return <>No items on shoppinglist</>
     }
+
+    const combinedShoppingList = combineIngredients(shoppinglist);
 
     return (
         <div>
             <h1>Shoppinglist</h1>
             <ul>
-                {shoppinglist.map(item => (    
-                    <li key={item.ingredient}>
+                {combinedShoppingList.map(item => (    
+                    <li key={item.id}>
                         {item.amount} {item.unit} of {item.ingredient} {item.recipy}
                         <button onClick={() => handleDelete(item.id)}>Delete</button>
                     </li>
