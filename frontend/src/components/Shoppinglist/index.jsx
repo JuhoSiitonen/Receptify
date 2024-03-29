cbc502d55b07d7afbfc54a502961592c19b3ca2a
Juho Siitonen <juho_tuomas@msn.com>
2024-03-30 00:42:24 +0200

    combined items in shopping list have base unit

--- frontend/src/components/Shoppinglist/index.jsx
+++ frontend/src/components/Shoppinglist/index.jsx
@@ -1,5 +1,6 @@
 import { useSelector, useDispatch } from 'react-redux';
 import { deleteFromShoppinglist } from '../../reducers/userReducer';
+import { combineIngredients } from '../../util/shoppingListFunctions';
 import ItemAddForm from './ItemAddForm';
 
 const Shoppinglist = () => {
@@ -10,42 +11,6 @@ const Shoppinglist = () => {
         dispatch(deleteFromShoppinglist(id))
         shoppinglist.filter(item => item.id !== id)
     }
-
-    const combineIngredients = (list) => {
-        const combinedList = [];
-        list.forEach(item => {
-            const existingItemIndex = combinedList.findIndex(elem => elem.ingredient === item.ingredient);
-            if (existingItemIndex !== -1) {
-                combinedList[existingItemIndex].amount = Number(combinedList[existingItemIndex].amount) + Number(convertToBaseUnit(item.amount, item.unit))
-            } else {
-                combinedList.push({ ...item, amount: convertToBaseUnit(item.amount, item.unit) });
-            }
-        });
-        return combinedList;
-    };
-
-    const convertToBaseUnit = (amount, unit) => {
-        switch (unit) {
-            case 'tbsp':
-                return amount * 15; 
-            case 'tsp':
-                return amount * 5; 
-            case 'kg':
-                return amount * 1000; 
-            case 'l':
-                return amount * 1000; 
-            case 'dl':
-                return amount * 100; 
-            case 'cup':
-                return amount * 240;
-            case 'pcs':
-            case '':
-                return amount; 
-            default:
-                return amount; 
-        }
-    };
-
 
     if (!shoppinglist) {
         return <>No items on shoppinglist</>
