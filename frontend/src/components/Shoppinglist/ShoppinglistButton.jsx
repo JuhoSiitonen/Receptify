3ef02754535c780f31a70b5538378e7ee2323331
Juho Siitonen <juho_tuomas@msn.com>
2024-03-30 00:47:03 +0200

    Add to shoppinglist button styles

--- frontend/src/components/Shoppinglist/ShoppinglistButton.jsx
+++ frontend/src/components/Shoppinglist/ShoppinglistButton.jsx
@@ -23,7 +23,7 @@ const ShoppinglistButton = ({ recipy }) => {
     }
 
     return (
-        <div>
+        <div className="action-button">
             <button onClick={handleClick}>Add to Shoppinglist</button>
         </div>
     )
