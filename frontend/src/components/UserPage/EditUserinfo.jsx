fb22565a11e76049b2be9e42fe01cfee828e7f6f
Juho Siitonen <juho_tuomas@msn.com>
2024-03-30 18:35:39 +0200

    component to call form components

---
+++ frontend/src/components/UserPage/EditUserinfo.jsx
@@ -0,0 +1,24 @@
+import EditAboutMe from "./EditAboutMe";
+import EditEmail from "./EditEmail";
+import Togglable from "../Togglable"
+
+const EditUserinfo = ({ user }) => {
+    let placeholder = 'Enter email';
+
+    if (user.email) {
+        placeholder = 'Edit email'
+    }
+
+    return (
+        <div>
+        <Togglable buttonLabel='Edit ebout me info'>
+            <EditAboutMe user={user} />
+        </Togglable>
+        <Togglable buttonLabel={placeholder}>
+            <EditEmail user={user} />
+        </Togglable>
+        </div>
+    )
+}
+
+export default EditUserinfo
