3fd16b8401edea6d5cbbb6e2d09f97c5f93131b8
Juho Siitonen <juho_tuomas@msn.com>
2024-03-31 18:54:03 +0300

    refactored login

---
+++ backend/controllers/login.js
@@ -0,0 +1,71 @@
+const bcrypt = require('bcrypt')
+const { User, Recipy, Rating } = require('../models');
+
+const login = async (req, res) => {
+    const { username, password } = request.body;
+    const user = await User.findOne({ where: { username }, 
+        include: [
+            { model: User, 
+              as: 'subscriptions', 
+              attributes: ["id", "username", "email", "about"],
+              through: {
+                attributes: []
+              }, },
+            { model: Recipy, 
+              as: 'userFavorites', 
+              attributes: ["id", "title"],
+              through: {
+                attributes: []
+              }, },
+          ],
+        }
+      );
+    const rated = await Rating.findAll({ 
+      where: { userId: user.id },
+      attributes: ["recipyId", "rating"],
+    });
+
+    const shoppinglist = [];
+
+    const email = user.email === null 
+    ? false 
+    : true;
+
+    const about = user.about === null
+    ? ''
+    : user.about;
+
+    const passwordCorrect = user === null
+    ? false
+    : await bcrypt.compare(password, user.password)
+
+    if (user && passwordCorrect) {
+        const sess = request.session;
+        sess.userId = user.id;
+        sess.username = user.username;
+        sess.admin = user.admin;
+        sess.subscriptions = JSON.stringify(user.subscriptions);
+        sess.userFavorites = JSON.stringify(user.userFavorites);
+        sess.rated = JSON.stringify(rated);
+        sess.shoppinglist = JSON.stringify(shoppinglist);
+        sess.email = email;
+        sess.about = about;
+        const returnUser = { 
+            id: user.id, 
+            username: user.username, 
+            admin: user.admin,
+            subscriptions: user.subscriptions,
+            userFavorites: user.userFavorites,
+            rated: rated,
+            shoppinglist: shoppinglist,
+            email: email,
+            about: about,
+         };
+        return response.status(200).json(returnUser);
+    }
+    return response.status(401).json({ error: 'invalid username or password' });
+}
+
+module.exports = {
+    login,
+}
