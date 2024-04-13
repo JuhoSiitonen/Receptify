bc1eb69d7d918521de778da3dee3c4b9eef37dc9
Juho Siitonen <104775829+JuhoSiitonen@users.noreply.github.com>
2024-04-13 22:28:57 +0000

    removed link to recipies from mainpage

--- frontend/src/components/Mainpage.jsx
+++ frontend/src/components/Mainpage.jsx
@@ -1,4 +1,3 @@
-import { Link } from 'react-router-dom'
 import './Mainpage.css'
 
 const Mainpage = () => {
@@ -7,7 +6,6 @@ const Mainpage = () => {
         <div className="mainpage-container">
           <h2>Welcome to Receptify!</h2>
           <h3>Here you can find recipes and add your own!</h3>
-          <p>Click <Link to="/recipes">here</Link> to see all recipes!</p>
         </div>
     )
 }
