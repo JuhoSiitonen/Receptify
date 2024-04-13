1c1342682c72b9ed135b92c0c055e41ba45ed10a
Juho Siitonen <104775829+JuhoSiitonen@users.noreply.github.com>
2024-04-13 21:48:28 +0000

    migration to add columns for number of recipies per user, number of subscribers, and recipy number of favorited.

---
+++ backend/migrations/20240413_01_subscriber_amount_and_favorite.js
@@ -0,0 +1,26 @@
+const { DataTypes } = require('sequelize')
+
+module.exports = {
+    up: async ({ context: queryInterface }) => {
+      await queryInterface.addColumn('users', 'subscribers', {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        defaultValue: 0,
+      })
+      await queryInterface.addColumn('users', 'number_of_recipes', {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        defaultValue: 0,
+      })
+      await queryInterface.addColumn('recipies', 'favorites', {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        defaultValue: 0,
+      })
+    },
+    down: async ({ context: queryInterface }) => {
+      await queryInterface.removeColumn('users', 'subscribers')
+      await queryInterface.removeColumn('users', 'number_of_recipes')
+      await queryInterface.removeColumn('recipies', 'favorites')
+    },
+  }
