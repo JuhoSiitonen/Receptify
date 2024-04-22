e9bd683ae6d945d6f47eb17bb8140a48dd407f87
Juho Siitonen <juho_tuomas@msn.com>
2024-04-22 12:20:39 +0300

    deleted all jest files

--- frontend/jest.setup.js
+++
@@ -1,9 +0,0 @@
-globalThis.fetch = require('isomorphic-fetch');
-import { server } from './src/tests/testServer.js'
-
-beforeAll(() => server.listen())
-
-afterEach(() => server.resetHandlers())
-
-afterAll(() => server.close())
-
--- frontend/setEnvVars.js
+++
@@ -1,2 +0,0 @@
-process.env.REACT_APP_BACKEND_URL='http://localhost:5173'
-process.env.NODE_ENV='development'
