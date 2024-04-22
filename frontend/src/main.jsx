77bcfca26ae77844bd4e2ac746930182de037d8f
Juho Siitonen <juho_tuomas@msn.com>
2024-04-22 12:43:30 +0300

    returning main components to normal, before msw worker setup

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,18 +3,6 @@ import { BrowserRouter as Router } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import reduxStore from './reducers/store'
 import App from './App'
-
-async function enableMocking() {
-  if (process.env.NODE_ENV !== 'development') {
-    return
-  }
- 
-  const { worker } = await import('./mocks/browser')
- 
-  return worker.start()
-}
-
-enableMocking().then(() => {
   
   ReactDOM.createRoot(document.getElementById('root')).render(
     <Router>
@@ -23,4 +11,4 @@ enableMocking().then(() => {
       </Provider>
     </Router>
   )
-})
+
--- frontend/src/util/apiClient.js
+++ frontend/src/util/apiClient.js
@@ -1,16 +1,8 @@
 import axios from 'axios'
 
-let apiClient
-
-if (process.env.NODE_ENV === 'development') {
-  apiClient = axios.create({
-    baseURL: process.env.REACT_APP_BACKEND_URL,
-  })
-} else {
-  apiClient = axios.create({
-    baseURL: process.env.REACT_APP_BACKEND_URL,
-    withCredentials: true,
+const apiClient = axios.create({
+  baseURL: process.env.REACT_APP_BACKEND_URL,
+  withCredentials: true,
 })
-}
 
 export default apiClient
