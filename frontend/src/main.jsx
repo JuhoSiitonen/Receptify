e401a9ae91baaf1e237fc387cdc5baa6d6786f29
Juho Siitonen <juho_tuomas@msn.com>
2024-04-22 12:17:40 +0300

    mock service worker activated in dev mode

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,10 +4,23 @@ import { Provider } from 'react-redux'
 import reduxStore from './reducers/store'
 import App from './App'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <Router>
-    <Provider store={reduxStore}>
-      <App />
-    </Provider>
-  </Router>
-)
+async function enableMocking() {
+  if (process.env.NODE_ENV !== 'development') {
+    return
+  }
+ 
+  const { worker } = await import('./mocks/browser')
+ 
+  return worker.start()
+}
+
+enableMocking().then(() => {
+  
+  ReactDOM.createRoot(document.getElementById('root')).render(
+    <Router>
+      <Provider store={reduxStore}>
+        <App />
+      </Provider>
+    </Router>
+  )
+})
