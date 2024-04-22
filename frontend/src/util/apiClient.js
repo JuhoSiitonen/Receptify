a2b811bc2a098b2080114726d4343b7fd8c5c664
Juho Siitonen <juho_tuomas@msn.com>
2024-04-22 12:15:27 +0300

    apiClient with credentials in producation, without in dev

--- frontend/src/util/apiClient.js
+++ frontend/src/util/apiClient.js
@@ -1,8 +1,16 @@
 import axios from 'axios'
 
-const apiClient = axios.create({
-  baseURL: process.env.REACT_APP_BACKEND_URL,
-  withCredentials: true,
+let apiClient
+
+if (process.env.NODE_ENV === 'development') {
+  apiClient = axios.create({
+    baseURL: process.env.REACT_APP_BACKEND_URL,
+  })
+} else {
+  apiClient = axios.create({
+    baseURL: process.env.REACT_APP_BACKEND_URL,
+    withCredentials: true,
 })
+}
 
 export default apiClient
